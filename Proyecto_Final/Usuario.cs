ee465e2065acc6be73627dd0fe665c6d03ce1ed3
Francovsky3 <154279672+Francovsky3@users.noreply.github.com>
2024-01-14 15:26:07 -0300

    Update Usuario.cs

--- Proyecto_Final/Usuario.cs
+++ Proyecto_Final/Usuario.cs
@@ -25,11 +25,11 @@ namespace Proyecto_Final
             this._mail = mail;
         }
 
-        public int Id { get { return _id; } set { _id = value; } }
-        public string Nombre { get { return _nombre; } set { _nombre = value; } }
-        public string Apellido { get { return _apellido; } set { _apellido = value; } }
-        public string NombreUsuario { get { return _nombreUsuario; } set { _nombreUsuario = value; } }
-        public string Contraseña { get { return _contraseña; } set { _contraseña = value; } }
-        public string Mail { get { return _mail; } set { _mail = value; } }
+        public int id { get { return _id; } set { _id = value; } }
+        public string nombre { get { return _nombre; } set { _nombre = value; } }
+        public string apellido { get { return _apellido; } set { _apellido = value; } }
+        public string nombreUsuario { get { return _nombreUsuario; } set { _nombreUsuario = value; } }
+        public string contraseña { get { return _contraseña; } set { _contraseña = value; } }
+        public string mail { get { return _mail; } set { _mail = value; } }
     }
 }
